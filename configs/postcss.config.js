5369868d3e96d0adabe08363046d46df15600b2a
Jun <junontwitchtv@gmail.com>
2020-06-03 21:54:32 -0400

    initial demo commit.

---
+++ configs/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = ctx => ({
+  'plugins': {
+    'postcss-import': {},
+    'postcss-preset-env': { stage: 1 },
+    'autoprefixer': {},
+    'cssnano': ctx.env === 'production' ? {} : false
+
+  }
+})
